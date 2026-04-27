9f61c5887ecfc47eb5c3d19366796cb537f76657
anamaski <anamatzkin10@gmail.com>
2026-04-27 18:44:42 -0300

    agrego contenido al navbar

---
+++ navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <span>LOGO</span>
+
+    <ul>
+      <li>Inicio</li>
+      <li>Sección</li>
+      <li>Contacto</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  justify-content: space-between;
+  background: #eee;
+  padding: 10px;
+}
+
+ul {
+  display: flex;
+  gap: 10px;
+  list-style: none;
+}
+</style>   
